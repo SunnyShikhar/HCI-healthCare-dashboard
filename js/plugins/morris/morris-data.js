13e1d6aa902ff68e3b7febe128ea7c9a7f81fb75
gaganchane <gchane@uwaterloo.ca>
2016-11-20 18:37:53 -0500

    Added line graph and donut graph

--- js/plugins/morris/morris-data.js
+++ js/plugins/morris/morris-data.js
@@ -2,6 +2,165 @@
 
 $(function() {
 
+    // Line Chart
+    Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'morris-line-chart',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: [{
+            d: '1990',
+            visits: 6
+        }, {
+            d: '',
+            visits: 8
+        }, {
+            d: '1991',
+            visits: 4
+        }, {
+            d: '1992',
+            visits: 4
+        }, {
+            d: '1993',
+            visits: 7
+        }, {
+            d: '1994',
+            visits: 3
+        }, {
+            d: '1995',
+            visits: 10
+        }, {
+            d: '1996',
+            visits: 3
+        }, {
+            d: '1997',
+            visits: 8
+        }, {
+            d: '1998',
+            visits: 6
+        }, {
+            d: '1999',
+            visits: 4
+        }, {
+            d: '2000',
+            visits: 8
+        }, {
+            d: '2001',
+            visits: 3
+        }, {
+            d: '2002',
+            visits: 4
+        }, {
+            d: '2003',
+            visits: 6
+        }, {
+            d: '2004',
+            visits: 5
+        }, {
+            d: '2005',
+            visits: 9
+        }, {
+            d: '2006',
+            visits: 4
+        }, {
+            d: '2007',
+            visits: 7
+        }, {
+            d: '2008',
+            visits: 3
+        }, {
+            d: '2009',
+            visits: 6
+        }, {
+            d: '2010',
+            visits: 8
+        }, {
+            d: '2011',
+            visits: 4
+        }, {
+            d: '2012',
+            visits: 5
+        }, {
+            d: '2013',
+            visits: 3
+        }, {
+            d: '2014',
+            visits: 2
+        }, {
+            d: '2015',
+            visits: 6
+        }, {
+            d: '2016',
+            visits: 4
+        }, /*{
+            d: '2012-10-29',
+            visits: 1420
+        }, {
+            d: '2012-10-30',
+            visits: 1529
+        }, {
+            d: '2012-10-31',
+            visits: 1892
+        }, */],
+        // The name of the data record attribute that contains x-visitss.
+        xkey: 'd',
+        // A list of names of data record attributes that contain y-visitss.
+        ykeys: ['visits'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Visits'],
+        // Disables line smoothing
+        smooth: false,
+        resize: true
+    });
+
+        // Donut Chart
+    Morris.Donut({
+        element: 'morris-donut-chart',
+        data: [{
+            label: "Dentist Visits",
+            value: 2
+        }, {
+            label: "Family Doctor Visits",
+            value: 5
+        }, {
+            label: "Orthodontist Visits",
+            value: 2
+        }],
+        resize: true
+    });
+
+    // Bar Chart
+    Morris.Bar({
+        element: 'morris-bar-chart',
+        data: [{
+            device: 'iPhone',
+            geekbench: 136
+        }, {
+            device: 'iPhone 3G',
+            geekbench: 137
+        }, {
+            device: 'iPhone 3GS',
+            geekbench: 275
+        }, {
+            device: 'iPhone 4',
+            geekbench: 380
+        }, {
+            device: 'iPhone 4S',
+            geekbench: 655
+        }, {
+            device: 'iPhone 5',
+            geekbench: 1571
+        }],
+        xkey: 'device',
+        ykeys: ['geekbench'],
+        labels: ['Geekbench'],
+        barRatio: 0.4,
+        xLabelAngle: 35,
+        hideHover: 'auto',
+        resize: true
+    });
+
     // Area Chart
     Morris.Area({
         element: 'morris-area-chart',
@@ -63,165 +222,5 @@ $(function() {
         hideHover: 'auto',
         resize: true
     });
-
-    // Donut Chart
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales",
-            value: 12
-        }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
-        }],
-        resize: true
-    });
-
-    // Line Chart
-    Morris.Line({
-        // ID of the element in which to draw the chart.
-        element: 'morris-line-chart',
-        // Chart data records -- each entry in this array corresponds to a point on
-        // the chart.
-        data: [{
-            d: '2012-10-01',
-            visits: 802
-        }, {
-            d: '2012-10-02',
-            visits: 783
-        }, {
-            d: '2012-10-03',
-            visits: 820
-        }, {
-            d: '2012-10-04',
-            visits: 839
-        }, {
-            d: '2012-10-05',
-            visits: 792
-        }, {
-            d: '2012-10-06',
-            visits: 859
-        }, {
-            d: '2012-10-07',
-            visits: 790
-        }, {
-            d: '2012-10-08',
-            visits: 1680
-        }, {
-            d: '2012-10-09',
-            visits: 1592
-        }, {
-            d: '2012-10-10',
-            visits: 1420
-        }, {
-            d: '2012-10-11',
-            visits: 882
-        }, {
-            d: '2012-10-12',
-            visits: 889
-        }, {
-            d: '2012-10-13',
-            visits: 819
-        }, {
-            d: '2012-10-14',
-            visits: 849
-        }, {
-            d: '2012-10-15',
-            visits: 870
-        }, {
-            d: '2012-10-16',
-            visits: 1063
-        }, {
-            d: '2012-10-17',
-            visits: 1192
-        }, {
-            d: '2012-10-18',
-            visits: 1224
-        }, {
-            d: '2012-10-19',
-            visits: 1329
-        }, {
-            d: '2012-10-20',
-            visits: 1329
-        }, {
-            d: '2012-10-21',
-            visits: 1239
-        }, {
-            d: '2012-10-22',
-            visits: 1190
-        }, {
-            d: '2012-10-23',
-            visits: 1312
-        }, {
-            d: '2012-10-24',
-            visits: 1293
-        }, {
-            d: '2012-10-25',
-            visits: 1283
-        }, {
-            d: '2012-10-26',
-            visits: 1248
-        }, {
-            d: '2012-10-27',
-            visits: 1323
-        }, {
-            d: '2012-10-28',
-            visits: 1390
-        }, {
-            d: '2012-10-29',
-            visits: 1420
-        }, {
-            d: '2012-10-30',
-            visits: 1529
-        }, {
-            d: '2012-10-31',
-            visits: 1892
-        }, ],
-        // The name of the data record attribute that contains x-visitss.
-        xkey: 'd',
-        // A list of names of data record attributes that contain y-visitss.
-        ykeys: ['visits'],
-        // Labels for the ykeys -- will be displayed when you hover over the
-        // chart.
-        labels: ['Visits'],
-        // Disables line smoothing
-        smooth: false,
-        resize: true
-    });
-
-    // Bar Chart
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            device: 'iPhone',
-            geekbench: 136
-        }, {
-            device: 'iPhone 3G',
-            geekbench: 137
-        }, {
-            device: 'iPhone 3GS',
-            geekbench: 275
-        }, {
-            device: 'iPhone 4',
-            geekbench: 380
-        }, {
-            device: 'iPhone 4S',
-            geekbench: 655
-        }, {
-            device: 'iPhone 5',
-            geekbench: 1571
-        }],
-        xkey: 'device',
-        ykeys: ['geekbench'],
-        labels: ['Geekbench'],
-        barRatio: 0.4,
-        xLabelAngle: 35,
-        hideHover: 'auto',
-        resize: true
-    });
-
-
+    
 });
